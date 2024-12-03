93a4b67b4a1947794f2bc1b0c55f888da8f3d823
CBO121311 <cbo@hotmail.es>
2024-12-04 00:22:19 +0100

    actualización de código

--- test_charfun.py
+++ test_charfun.py
@@ -1,16 +1,10 @@
 """
 test_charfun.py
 
-Archivo que contiene un conjunto de pruebas unitarias para verificar el correcto funcionamiento
-de la función `esPalindromo`, definida en el módulo `charfun.py`.
+Archivo con pruebas unitarias de la función 'esPalindromo' del módulo 'charfun.py'.
+Las pruebas están diseñadas para validar diferentes casos
 
-Las pruebas están diseñadas para validar diferentes casos, incluyendo:
-- Cadenas vacías o con espacios.
-- Cadenas de un solo carácter.
-- Palíndromos simples y complejos (incluyendo tildes, mayúsculas y símbolos).
-- Cadenas que no son palíndromas.
-
-Ultima Modificación. 01/12/2024
+Ultima Modificación. 04/12/2024
 
 Autor. Alejandro López Calderón
 """
@@ -28,14 +22,14 @@ class TestEsPalindromo(unittest.TestCase):
         self.assertEqual(esPalindromo("    "), False)
     
     def test_un_caracter(self):
-        # Un solo carácter no debería ser palíndromo
-        self.assertEqual(esPalindromo("c"), False)
+        # Un solo carácter
+        self.assertEqual(esPalindromo("v"), False)
 
     def test_palindromos(self):
         # Cadenas palíndromas
         self.assertEqual(esPalindromo("Somos o no somos"), True)
         self.assertEqual(esPalindromo("Luz azul"), True)
-        self.assertEqual(esPalindromo("La tele letal"), True)
+        self.assertTrue(esPalindromo("12321"))
 
     def test_no_palindromos(self):
         # Cadenas no palíndromas
@@ -44,8 +38,8 @@ class TestEsPalindromo(unittest.TestCase):
 
     def test_cadenas_con_tildes_y_simbolos(self):
         # Cadenas con tildes y caracteres especiales
-        self.assertTrue(esPalindromo("ÁÉÍÚ úíéá"), True)
         self.assertEqual(esPalindromo("¡Abusón, acá no suba!"), True)
+        self.assertTrue(esPalindromo("¿A mamá Roma le aviva el amor a mamá?"), True) 
         self.assertTrue(esPalindromo("Sólos"), True) 
 
 if __name__ == "__main__":
